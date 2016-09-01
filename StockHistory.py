7c038721db7c9679cd53a842ff61eacc066c6447
xhacekrtxl <xhackertxl@gmail.com>
2016-09-01 21:28:11 +0800

    修改oracle字段CLOB无法创建索引

--- StockHistory.py
+++ StockHistory.py
@@ -6,6 +6,9 @@ from Average import  *
 #create user tacker identified by tacker;
 #grant session to tacker;//创建李四访问数据库权限
 #grant connect,resource,dba to tacker;
+#报字段CLOB无法创建索引
+#gedit /public/tools/pyenv/versions/anaconda3-4.1.0/lib/python3.5/site-packages/sqlalchemy/sql/compiler.py
+
 
 conn = cx_Oracle.connect('tacker/tacker@localhost:1521/orcl')
 cursor = conn.cursor()
@@ -20,6 +23,13 @@ conn.close()
 engine = engine = create_engine('oracle+cx_oracle://tacker:tacker@orcl')
 
 df = ts.get_stock_basics()
+#存入数据库
+df.to_sql('stock_code_list',engine)
+
+
+df.to_sql('tick_data',engine)
+
+
 date = df.ix['600848']['timeToMarket'] #上市日期YYYYMMDD
 
 #本接口还提供大盘指数的全部历史数据，调用时，请务必设定index参数为True,由于大盘指数不存在复权的问题，故可以忽略autype参数。
@@ -27,27 +37,21 @@ date = df.ix['600848']['timeToMarket'] #上市日期YYYYMMDD
 df = ts.get_h_data('002337') #前复权
 
 #存入数据库
-df.to_sql('days_002337',engine)
+#df.to_sql('days_002337',engine)
 
-ts.get_h_data('002337', autype='hfq') #后复权
-ts.get_h_data('002337', autype=None) #不复权
-ts.get_h_data('002337', start='2015-01-01', end='2015-03-16') #两个日期之间的前复权数据
+# ts.get_h_data('002337', autype='hfq') #后复权
+# ts.get_h_data('002337', autype=None) #不复权
+# ts.get_h_data('002337', start='2015-01-01', end='2015-03-16') #两个日期之间的前复权数据
 
-ts.get_h_data('399106', index=True) #深圳综合指数
+# ts.get_h_data('399106', index=True) #深圳综合指数
 
 
-
-df = ts.get_tick_data('600848', date='2014-12-22')
+#df = ts.get_tick_data('600848', date='2014-12-22')
 
 #存入数据库
-df.to_sql('tick_data',engine)
+#df.to_sql('tick_data',engine)
 
-print('finish')
-
-
-
-
-
+# print('finish')
 
 #追加数据到现有表
 #df.to_sql('tick_data',engine,if_exists='append')
